9b58b0ea2e7f6311791568e4feb2615d1144bfdb
Andry <muha_pk@mail.ru>
2024-11-08 17:34:01 +0100

    connected apollo client

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -7,9 +7,23 @@ import { H1, Underline } from 'shared/ui/CustomText/CustomText';
 import { Container } from 'shared/ui/Container/Container';
 import { Lang } from 'shared/lang';
 
+import { useQuery } from '@apollo/client';
+import { GET_ALL_USERS, GET_USER } from 'shared/api/graphql/queries/getUsers';
+
+
+
 export const Login: FC = () => {
 
     const { form, login } = Lang()
+
+
+    const { data, loading, error } = useQuery(GET_USER, {variables: {id: 2}});
+
+    if (loading) return console.log('loading')
+    if (error) return console.log('error: ' + error.message)
+
+    if (data) console.log('data ' + JSON.stringify(data, null, 2))
+
 
     const { control, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
